19dec8062d47eacd6e77a9b76d9679e1682f7d3d
AnaghaHM <75258376+Anagha-hm@users.noreply.github.com>
2021-12-06 14:14:49 +0530

    value error expected 2d got 1d

--- incrementalModels/incLR.py
+++ incrementalModels/incLR.py
@@ -11,7 +11,7 @@ from pyspark.ml.feature import StringIndexer
 from pyspark.ml import Pipeline
 from pyspark.sql.functions import col, size
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
-
+from sklearn import preprocessing
 import pickle
 
 import numpy as np
@@ -22,13 +22,16 @@ def logRegression(df):
 	dfs = np.array(df.select('Sentiment').collect())
 	dft = np.array(df.select('Tweet').collect())
 	
+	le = preprocessing.LabelEncoder()
+	dft = le.fit_transform(np.array(df.select('Tweet').collect()))
+	
 	dfs_train, dfs_val, dft_train, dft_val = train_test_split(dfs, dft, test_size = 0.1)
+	sgd = SGDClassifier()
+	dfs_train.reshape(-1,1)
+	dft_train.reshape(-1,1)
+	sgd.partial_fit(dft_train, dfs_train, classes = [0,4])
+	pred_s = sgd.predict(dft_val)
 	
-	classifier = SGDClassifier(loss = 'log', max_iter = 1000, tol = 1e-3)
-	
-	clf = make_pipeline(StandardScaler(), classifier)
-	clf.fit(dfs_train, np.ravel(dft_train))
-	
-	accuracy = clf.score(dfs_val, dft_val)
+	accuracy = accuracy_score( pred_s,dfs_val)
 	
 	return accuracy
