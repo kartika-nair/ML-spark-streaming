d462576e54b644517d864a80f30e27a371bfba3f
AnaghaHM <75258376+Anagha-hm@users.noreply.github.com>
2021-12-06 18:12:00 +0530

    gives huge -ve score

--- incrementalModels/incKM.py
+++ incrementalModels/incKM.py
@@ -5,65 +5,29 @@ from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import train_test_split
-
+from sklearn.cluster import KMeans
+from pyspark.sql.types import *
+from pyspark.ml.feature import StringIndexer,VectorAssembler 
+from pyspark.ml import Pipeline
 from pyspark.ml.evaluation import ClusteringEvaluator
-
+import pickle
+import joblib
 import numpy as np
 
+def models(df):
+	indexer = StringIndexer(inputCol="Tweet", outputCol="Tweets_Indexed", stringOrderType='alphabetAsc')
+	pipeline = Pipeline(stages=[indexer])
+	pipelineFit = pipeline.fit(df)
+	dataset = pipelineFit.transform(df)
 
-def kmm(df):
+	new_df=dataset.select(['Tweets_Indexed'])
+	new_df_target=dataset.select(['Sentiment'])
 	
-	accuracy = -1
+	x=np.array(new_df.select('Tweets_Indexed').collect())
+	y=np.array(new_df_target.select('Sentiment').collect())
 	
-	df1 = np.array(df.collect())
-	df_sent = np.array(df.select('Sentiment').collect())
-	
-	le = LabelEncoder()
-	df_twt = le.fit_transform(np.array(df.select('Tweet').collect()))
-	
-	sent_train, sent_test, twt_train, twt_test = train_test_split(df_sent, df_twt, test_size = 0.1, random_state = 0)
-	
-	kmeans = MiniBatchKMeans(n_clusters = 10)
-	model = kmeans.partial_fit(sent_train, np.ravel(twt_train))
-	
-	predictions = kmeans.predict(sent_test)
-	
-	accuracy = accuracy_score(np.ravel(twt_test), predictions)
-	
-	return accuracy
-'''import sklearn as sk
-from sklearn.model_selection import train_test_split
-from sklearn.cluster import MiniBatchKMeans
-from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import train_test_split
+	model_kmm=KMeans(n_clusters=1000, random_state=0).fit(x,y)
 
-from pyspark.ml.evaluation import ClusteringEvaluator
-
-import numpy as np
-
-
-def kmm(df):
-	
-	accuracy = -1
-	
-	df1 = np.array(df.collect())
-	df_sent = np.array(df.select('Sentiment').collect())
-	
-	le = LabelEncoder()
-	df_twt = le.fit_transform(np.array(df.select('Tweet').collect()))
-	
-	sent_train, sent_test, twt_train, twt_test = train_test_split(df_sent, df_twt, test_size = 0.1, random_state = 0)
-	
-	kmeans = MiniBatchKMeans(n_clusters = 10)
-	y = twt_train.ravel()
-	train_y = np.array(y).astype(int)
-	model = kmeans.partial_fit(sent_train, train_y)
-	
-	predictions = kmeans.predict(sent_test)
-	p = predictions.ravel()
-	pr = np.array(p).astype(int)
-	accuracy = accuracy_score(twt_test, p)
-	
-	return accuracy'''
+	joblib.dump(model_kmm,'KMM.pkl')
+	result1 = model_kmm.score(x,y.ravel())
+	return result1
