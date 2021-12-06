db7bd67c782fe6845c0ec5dc3ae4448ac049531f
Ananya Uppal <55040997+ananya2712@users.noreply.github.com>
2021-12-06 17:05:45 +0530

    Add files via upload

--- incrementalModels/incLR.py
+++ incrementalModels/incLR.py
@@ -11,27 +11,47 @@ from pyspark.ml.feature import StringIndexer
 from pyspark.ml import Pipeline
 from pyspark.sql.functions import col, size
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
-from sklearn import preprocessing
+
+from sklearn import preprocessing 
+from pyspark.sql.types import *
+from pyspark.ml.feature import StringIndexer,VectorAssembler #OneHotEncoderEstimator
+import sklearn.linear_model as lm 
 import pickle
 
 import numpy as np
 
 
 def logRegression(df):
-	
 	dfs = np.array(df.select('Sentiment').collect())
 	dft = np.array(df.select('Tweet').collect())
 	
-	le = preprocessing.LabelEncoder()
-	dft = le.fit_transform(np.array(df.select('Tweet').collect()))
-	
 	dfs_train, dfs_val, dft_train, dft_val = train_test_split(dfs, dft, test_size = 0.1)
-	sgd = SGDClassifier()
-	dfs_train.reshape(-1,1)
-	dft_train.reshape(-1,1)
-	sgd.partial_fit(dft_train, dfs_train, classes = [0,4])
-	pred_s = sgd.predict(dft_val)
 	
-	accuracy = accuracy_score( pred_s,dfs_val)
+	classifier = SGDClassifier(loss = 'log', max_iter = 1000, tol = 1e-3)
+	
+	clf = make_pipeline(StandardScaler(), classifier)
+	clf.fit(dfs_train, np.ravel(dft_train))
+	
+	accuracy = clf.score(dfs_val, dft_val)
 	
 	return accuracy
+
+def model(df):
+	
+	indexer = StringIndexer(inputCol="Tweet", outputCol="Tweets_Indexed", stringOrderType='alphabetAsc')
+	pipeline = Pipeline(stages=[indexer])
+	pipelineFit = pipeline.fit(df)
+	dataset = pipelineFit.transform(df)
+
+	new_df=dataset.select(['Tweets_Indexed'])
+	new_df_target=dataset.select(['Sentiment'])
+	
+	x=np.array(new_df.select('Tweets_Indexed').collect())
+	y=np.array(new_df_target.select('Sentiment').collect())
+	
+	model_lm=lm.LogisticRegression(warm_start=True)
+	model_lm=model_lm.fit(x,y.ravel())
+	
+	joblib.dump(model_lm,'Logistic_Regression.pkl')
+	result1=model_lm.score(x, y)
+	return result1
