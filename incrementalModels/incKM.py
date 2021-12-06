f721c61118a47e35de19b43047a1806825403510
AnaghaHM <75258376+Anagha-hm@users.noreply.github.com>
2021-12-06 20:01:41 +0530

    final kmm-works

--- incrementalModels/incKM.py
+++ incrementalModels/incKM.py
@@ -5,16 +5,17 @@ from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import train_test_split
-from sklearn.cluster import KMeans
+from sklearn.cluster import MiniBatchKMeans
 from pyspark.sql.types import *
 from pyspark.ml.feature import StringIndexer,VectorAssembler 
 from pyspark.ml import Pipeline
 from pyspark.ml.evaluation import ClusteringEvaluator
+from sklearn.metrics import accuracy_score
 import pickle
 import joblib
 import numpy as np
 
-def models(df):
+def KMeansClustering(df):
 	indexer = StringIndexer(inputCol="Tweet", outputCol="Tweets_Indexed", stringOrderType='alphabetAsc')
 	pipeline = Pipeline(stages=[indexer])
 	pipelineFit = pipeline.fit(df)
@@ -25,9 +26,9 @@ def models(df):
 	
 	x=np.array(new_df.select('Tweets_Indexed').collect())
 	y=np.array(new_df_target.select('Sentiment').collect())
-	
-	model_kmm=KMeans(n_clusters=1000, random_state=0).fit(x,y)
-
+	dfs_train, dfs_val, dft_train, dft_val = train_test_split(x, y, test_size = 0.1)
+	model_kmm=MiniBatchKMeans(n_clusters=2, random_state=0).partial_fit(dft_train,dfs_train)
+	pred = model_kmm.predict(dft_val)
 	joblib.dump(model_kmm,'KMM.pkl')
-	result1 = model_kmm.score(x,y.ravel())
-	return result1
+	result1 = accuracy_score(dft_val,pred)
+	return abs(result1)
